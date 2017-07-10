ef88eb02bf2023d8adad0a9c55e29ee10b99a5ee
Paige Miller <paige_9753@hotmail.com>
2017-07-10 14:30:33 -0400

    Added cat program

---
+++ IntroToOOP/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    class Cat
+    {
+        //Inside our class, we create a blueprint for our object.
+        //Classes are tmeplates in OOP that we use to create objects.
+        //They should always be named in Pascal Case.
+    }
+}
--- IntroToOOP/Program.cs
+++ IntroToOOP/Program.cs
@@ -10,6 +10,20 @@ namespace IntroToOOP
     {
         static void Main(string[] args)
         {
+            //When we initalize a new object, it is called INSTANTIATING (creating a new instance
+            //An object needs to be instantiated befoe it can be used
+            //All objects of a specific type are created from the same template - CLASS
+            //All objects from a class will have the same number and type of states, BUT...
+            //each instance can have its own unique value.
+
+            //Format we use to instantiate an object looks like this:
+            //ClassName objectName = new ClassName();
+            Cat mittens = new Cat();
+            //To create a new object, we use a special method called a CONSTRUCTOR
+
+            //Thinking about objects in real life, create (either hand-write or type)
+            //three examples of objects. Your objects should have five states (characteristics)
+            //and five behaviors.
         }
     }
 }
